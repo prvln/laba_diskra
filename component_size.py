d31768fdd6cf4c2a1fe164bdda2123442ef7672a
Angelina <centavra2012@mail.ru>
2019-04-13 00:20:39 +0300

    can read and take parametrs

---
+++ component_size.py
@@ -0,0 +1,33 @@
+a = []
+z = []
+out = []
+
+L = 0
+
+for i in open('test1.in'):
+    a.append(i[:-1])
+    b = i
+    z += b.split()
+
+for i in open('test6.out'):
+    a.append(i[:-1])
+    b = i
+    out += b.split()
+
+n = int(z[0])           #int(input("Введите кол-во вершин: "))
+m = int(z[1])           #int(input("Введите кол-во рёбер: "))
+v = int(z[2])           #int(input("Введите кол-во пар номеров смежных вершин: "))
+
+out = int(out[0])
+
+mas_x = []              # массив х1,...,xn
+mas_y = [] # массив у1,...,yn
+
+
+for i in range(3,len(z)):             # разбиваем на 2 массива х и у
+    if i % 2 !=0:
+        mas_x.append(int(z[i]))
+    else:
+        mas_y.append(int(z[i]))
+
+print(out)
